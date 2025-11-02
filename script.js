61f327dcc012e040c12884e97983b8dac7749e17
PruthviRaj2705 <pruthvirajrc2705@gmail.com>
2025-11-02 14:33:28 +0000

    final frontend version

---
+++ script.js
@@ -0,0 +1,20 @@
+// Highlight current section link while scrolling
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav ul li a");
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 100;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
